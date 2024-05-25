75fd69c2c61611a18dc25ee7de676625fae1066b
Octa44B <92257865+Octa44B@users.noreply.github.com>
2024-05-25 16:03:40 +0200

    Update integral.py
    additional comments

--- integral.py
+++ integral.py
@@ -3,13 +3,15 @@ import threading
 import time
 
 s=0
-a,b,dx= 1,2,1e-5
-nThreads=4
+a,b= 1,2
+nThreads=4 #Number of threads to operate on, the functions will work on their own without multithreading so you can copy paste them in your code
 lock = threading.Lock()
 
+#Function to integrate
 def fun(x):
     return x**2
 
+### INTEGRALS APPROXIMATION METHODS
 def midintegral(a,b,fun,n):
     """
     Integral of f from a to b with mid rectangles method
@@ -25,20 +27,7 @@ def midintegral(a,b,fun,n):
     with lock:
         s += ls
 
-time1=time.time()
-threads=[threading.Thread(target=midintegral, args=(a+((i*(b-a))/nThreads),a+((i+1)*(b-a)/nThreads),fun,33333,)) for i in range(nThreads)]
-
-for thread in threads:
-    thread.start()
-for thread in threads:
-    thread.join()
-
-print(time.time()-time1)
-print(s)
-
-
-s=0
-
+dx=1e-5
 def trapintegral(a:float,b:float,f,dx:float):
     """
     Integral of f from a to b with traps method
@@ -54,6 +43,23 @@ def trapintegral(a:float,b:float,f,dx:float):
     ls+=bmoddx*(f(b-bmoddx)+f(b))/2
     with lock:
         s+=ls
+
+### TO TEST WHICH METHODS IS FASTER AND MORE ACCURATE
+time1=time.time()
+threads=[threading.Thread(target=midintegral, args=(a+((i*(b-a))/nThreads),a+((i+1)*(b-a)/nThreads),fun,33333,)) for i in range(nThreads)]
+
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+
+print(time.time()-time1)
+print(s)
+
+
+s=0
+
+
 
 time1=time.time()
 threads=[threading.Thread(target=trapintegral, args=(a+((i*(b-a))/nThreads),a+((i+1)*(b-a)/nThreads),fun,dx,)) for i in range(nThreads)]
